367217cc1fb22734c28f0597d54d341c617946b6
MSP95 <patilmanish1910@gmail.com>
2018-02-04 23:32:47 -0500

    Documentation

--- hw04/calc/lib/calc.ex
+++ hw04/calc/lib/calc.ex
@@ -15,17 +15,17 @@ defmodule Calc do
 
   """
   # ///////////////////////////////////////////////////////////////////////////
-
+  # evaluates the given string and returns an integer value
   def eval(exp) do
     opd_stack = []
     opr_stack = []
     parse_into_list(exp)
-      |>traverse(opd_stack,opr_stack)
-      |> String.to_integer()
+    |>traverse(opd_stack,opr_stack)
+    |> String.to_integer()
   end
 
   # ///////////////////////////////////////////////////////////////////////////
-
+  # loop to keep the program endlessly running, waiting each time for user input.
   def main do
     IO.gets("\nEnter Expression Here: ")
     |> eval()
@@ -34,7 +34,7 @@ defmodule Calc do
   end
 
   # ///////////////////////////////////////////////////////////////////////////
-
+  # Converts given Arithmetic Expression in string form into a list.
   def parse_into_list(exp) do
     String.replace(exp, ~r/[\*\/\-\+\(\)]/," \\0 ")
     |>String.trim()
@@ -44,12 +44,16 @@ defmodule Calc do
 
   # ///////////////////////////////////////////////////////////////////////////
   # Stack Implementation:
-
+  # Returns the top value of the stack
   def top(stack) do
-    [last_in | _] = stack
-    last_in
+    if length(stack)>0 do
+      [last_in | _] = stack
+      last_in
+    else
+      raise("Invalid Arithmetic Expression Entered.")
+    end
   end
-
+  # Returns a stack without it's top value along with the removed top value.
   def pop(stack) do
     if length(stack)>0 do
       [last_in | rest] = stack
@@ -57,14 +61,15 @@ defmodule Calc do
     else
       raise("Invalid Arithmetic Expression Entered.")
     end
-
   end
-
+  # returns a stack containing item as the top and the input stack as
+  # the rest of the stack.
   def push(stack, item) do
     [item | stack]
   end
 
   # ///////////////////////////////////////////////////////////////////////////
+  # Computes the leftover stack elements after the whole Expression is traversed.
   def compute_rest(opd_stack, opr_stack) do
     if length(opr_stack)==0 do
       {opd_stack,opr_stack}
@@ -75,7 +80,8 @@ defmodule Calc do
   end
 
   # ///////////////////////////////////////////////////////////////////////////
-
+  # Computes the value inside any parentheses, and returns the updated
+  # operator and operand stacks.
   def recursively_compute(opd_stack, opr_stack) do
     topval = top(opr_stack)
     if topval=="(" do
@@ -88,7 +94,9 @@ defmodule Calc do
   end
 
   # ////////////////////////////////////////////////////////////////////////////
-
+  # Computes a sub Expression containing two the popped out operands from
+  # operator stack and a popped out operator from the operator stack.
+  # Pushes the new value into the operand stack and returns both the stacks.
   def compute(opd_stack, opr_stack) do
     {opd1, opd_stack} = pop(opd_stack)
     {opr, opr_stack} = pop(opr_stack)
@@ -107,7 +115,9 @@ defmodule Calc do
   end
 
   # ///////////////////////////////////////////////////////////////////////////
-
+  # Checks for precedence of operators and parentheses.
+  # Returns true if precedence of stack top is less than the precedence of the
+  # current value in focus.
   def check_precedence(stackTop,currentVal) do
     precedence = %{"("=>1, "*"=>5,"/"=>4,"+"=>3,"-"=>2}
     precedence = case {stackTop, currentVal} do
@@ -119,7 +129,8 @@ defmodule Calc do
   end
 
   # ///////////////////////////////////////////////////////////////////////////
-
+  # Handles the operation, when an operator is found while traversing the
+  # Expression.
   def handle_operator(opd_stack, opr_stack, this) do
     if length(opr_stack)==0 or check_precedence(top(opr_stack),this) do
       opr_stack = push(opr_stack, this)
@@ -131,7 +142,9 @@ defmodule Calc do
   end
 
   # ///////////////////////////////////////////////////////////////////////////
-
+  # Traverses through the whole Arithmetic Expression and returns the
+  # top of the operand stack at the end when there is only one value left
+  # in operator stack.
   def traverse(exp, opd_stack, opr_stack) do
     [this | rest] = exp
     {opd_stack, opr_stack} = cond do
@@ -153,7 +166,11 @@ defmodule Calc do
 
     if length(rest)==0 do
       {opd_stack,_} = compute_rest(opd_stack, opr_stack)
-      top(opd_stack)
+      if length(opd_stack)==1 do
+        top(opd_stack)
+      else
+        raise "Invalid Arithmetic Expression Entered"
+      end
     else
       traverse(rest, opd_stack, opr_stack)
     end
